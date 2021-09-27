b454fe06a241bed5962c4a293c6f96236faf61e2
syx9527 <1275241305@q.com>
2021-09-27 11:34:38 +0800

    update WSGI

--- WSGI/app.py
+++ WSGI/app.py
@@ -3,4 +3,4 @@ def simple_app(environ, start_response):
     status = "200 OK"
     response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
-    return [b'Hello World!\n\n邵曰信的网页']
+    return [b'Hello World!\n\nsyx']

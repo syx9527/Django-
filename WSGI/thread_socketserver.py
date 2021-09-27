46e5962fb1b70246b1322395d89dee82c77eb906
syx9527 <1275241305@q.com>
2021-09-27 14:12:34 +0800

    update WSGI

--- WSGI/thread_socketserver.py
+++ WSGI/thread_socketserver.py
@@ -42,7 +42,7 @@ def main():
 
     # 设置端口可复用，保证我们每次按Ctrl+C组合键后，快速重启
     serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    serversocket.bind(('47.103.205.80', 8000))
+    serversocket.bind(('0.0.0.0', 8000))
     serversocket.listen(10)  # 设置backlog--socket链接最大排队数量
     print("http://47.103.205.80:8000")
     serversocket.setblocking(0)  # 设置为非阻塞模式

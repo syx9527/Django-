314c799e5c4cfb8722fa815905be41a90440518b
syx9527 <1275241305@q.com>
2021-09-27 11:26:53 +0800

    update WSGI

---
+++ WSGI/app.py
@@ -0,0 +1,6 @@
+def simple_app(environ, start_response):
+    """Sim"""
+    status = "200 OK"
+    response_headers = [('Content-type', 'text/html')]
+    start_response(status, response_headers)
+    return [b'Hello World!\n']

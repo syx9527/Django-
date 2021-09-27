9879b92f9347ce53b1ceb9b1bc5d2bf7e9a793d5
syx9527 <1275241305@q.com>
2021-09-27 14:29:50 +0800

    update WSGI

--- WSGI/app.py
+++ WSGI/app.py
@@ -4,3 +4,16 @@ def simple_app(environ, start_response):
     response_headers = [('Content-type', 'text/html')]
     start_response(status, response_headers)
     return [b'Hello World!\n\nsyx\n']
+
+
+class AppClass(object):
+    status = "200 OK"
+    response_headers = [('Content-type', 'text/html')]
+
+    def __call__(self, environ, start_response):
+        print(environ, start_response)
+        start_response(self.status, self.response_headers)
+        return [b'Hello AppClass.__call__\n']
+
+
+application = AppClass()
